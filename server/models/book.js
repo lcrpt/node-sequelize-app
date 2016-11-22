785b7e8650f886cd319d41745c142d94fcf4cb20
leolassence <leolassence@gmail.com>
2016-11-22 19:09:57 +0100

    added book models with sequelize-cli commands

---
+++ server/models/book.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Book = sequelize.define('Book', {
+    name: DataTypes.STRING,
+    isbn: DataTypes.INTEGER,
+    publication_date: DataTypes.DATE,
+    description: DataTypes.TEXT,
+    author_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Book;
+};
