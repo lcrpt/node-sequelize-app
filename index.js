9d08e4a3003fdb56a5a3e712049c39131d5b4fec
leolassence <leolassence@gmail.com>
2016-11-22 22:45:51 +0100

    added API Endpoints for authors and books

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import authors from './server/controllers/authors';
 const app = express();
 
 app.get('/authors', authors.index);
+app.get('/authors/:id', authors.show);
+app.post('/authors', authors.create);
+app.put('/authors', authors.update);
+app.delete('/authors', authors.delete);
+
+app.get('/books', books.index);
+app.get('/books/:id', books.show);
+app.post('/books', books.create);
+app.delete('/books', books.delete);
 
 app.set('port', process.env.PORT || 3000);
 
