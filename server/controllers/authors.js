c8df33954bfc568b57323c4fe5d27c8a6948cb62
leolassence <leolassence@gmail.com>
2016-11-22 19:08:08 +0100

    added authors CRUD with sequelize

---
+++ server/controllers/authors.js
@@ -0,0 +1,68 @@
+Author = require('../models/').Author;
+Book = require('../models').Book;
+
+export default {
+  index(req, res) {
+    Author.findAll({
+      include: Book,
+    })
+    .then((authors) => {
+      res.status(200).json(authors);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
+
+  show(req, res) {
+    Author.findById(req.params.id, {
+      include: Book,
+    })
+    .then((author) => {
+      res.status(200).json(author);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
+
+  create(req, res) {
+    Author.create(req.body, {
+      include: Book,
+    })
+    .then((newAuthor) => {
+      res.status(200).json(newAuthor);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
+
+  update(req, res) {
+    Author.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((updatedAuthor) => {
+      res.status(200).json(updatedAuthor);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
+
+  delete(req, res) {
+    Author.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((deletedAuthor) => {
+      res.status(200).json(deletedAuthor);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
+};
